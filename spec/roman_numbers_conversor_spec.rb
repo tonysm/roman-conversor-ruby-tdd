2bba7afc75a0aeaa38d8fb6211e3309c40dde901
Tony Messias <tony@gabstr.com>
2014-06-24 17:20:11 -0300

    fix method name to fit the ruby way

--- spec/roman_numbers_conversor_spec.rb
+++ spec/roman_numbers_conversor_spec.rb
@@ -6,21 +6,21 @@ RSpec.describe RomanNumbersConversor do
     end
 
     it "converts numbers to roman" do
-        assertRoman 1, "I"
-        assertRoman 2, "II"
-        assertRoman 3, "III"
-        assertRoman 4, "IV"
-        assertRoman 5, "V"
-        assertRoman 6, "VI"
-        assertRoman 7, "VII"
-        assertRoman 9, "IX"
-        assertRoman 10, "X"
-        assertRoman 20, "XX"
-        assertRoman 2014, "MMXIV"
-        assertRoman 19, "XIX"
+        assert_roman 1, "I"
+        assert_roman 2, "II"
+        assert_roman 3, "III"
+        assert_roman 4, "IV"
+        assert_roman 5, "V"
+        assert_roman 6, "VI"
+        assert_roman 7, "VII"
+        assert_roman 9, "IX"
+        assert_roman 10, "X"
+        assert_roman 20, "XX"
+        assert_roman 2014, "MMXIV"
+        assert_roman 19, "XIX"
     end
 
-    def assertRoman number, romain
+    def assert_roman number, romain
         expect(@roman.fromNumbers(number)).to eq(romain)
     end
 end
